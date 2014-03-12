1f590c49cd30802096437243bf66fca1ae0b78d9
Ben Ng <me@benng.me>
2014-03-12 01:25:41 -0500

    Add testing stuff

--- test.js
+++ test.js
@@ -2,6 +2,15 @@ var sleepsort = require('./index.js')
   , assert = require('assert');
 
 sleepsort([1,3,2], function (result) {
-  assert.deepEqual(result, [1, 2, 3]);
+  try {
+    assert.deepEqual(result, [1, 2, 3]);
+  }
+  catch(e) {
+    console.log(e.message);
+    console.log(e.stack);
+    process.exit(1);
+  }
+
   console.log('It works.');
+  process.exit(0);
 });
